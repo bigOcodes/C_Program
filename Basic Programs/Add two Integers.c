e60fdf909337ca6add27d8d3739ed8ea69576ca5
_pravakar <61041599+bigOcodes@users.noreply.github.com>
2020-05-07 20:32:43 +0530

    Create Add two Integers.c

---
+++ Basic Programs/Add two Integers.c
@@ -0,0 +1,30 @@
+//Program to Add Two Integers
+
+#include <stdio.h>
+int main()
+{    
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+//    Output
+//Enter two integers: 12
+//11
+//12 + 11 = 23
+
+/*
+In this program, the user is asked to enter two integers. These two integers are stored in variables number1 and number2 respectively.
+
+printf("Enter two integers: ");
+scanf("%d %d", &number1, &number2);
+Then, these two numbers are added using the + operator, and the result is stored in the sum variable.
+
+sum = number1 + number2;
+*/
